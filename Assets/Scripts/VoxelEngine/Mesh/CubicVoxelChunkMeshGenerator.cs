44495bc67afb6e67055f9b4b5f7ce896b9f2fdb4
Matthew Taylor <wrapperup4@gmail.com>
2021-01-17 12:01:21 -0500

    Voxel Engine: Move cull lookup table outside of method

--- Assets/Scripts/VoxelEngine/Mesh/CubicVoxelChunkMeshGenerator.cs
+++ Assets/Scripts/VoxelEngine/Mesh/CubicVoxelChunkMeshGenerator.cs
@@ -11,77 +11,7 @@ namespace VoxelEngine.Mesh
     [CreateAssetMenu(fileName = "CubicGenerator.asset", menuName = "VoxelEngine/Mesh/Cubic Generator")]
     public class CubicVoxelChunkMeshGenerator : VoxelChunkMeshGeneratorBase
     {
-        public override VoxelMeshRenderData GenerateVoxel(Vector3Int position, ref VoxelData voxel, VoxelData[] neighbors)
-        {
-            (byte, byte) data = VoxelData.UnpackData(voxel.info);
-
-            var intensity = data.Item1;
-            var materialIndex = data.Item2;
-            
-            if (intensity < 8)
-            {
-                return new VoxelMeshRenderData(new List<Vector3>(), new List<int>());
-            }
-
-            var x = position.x;
-            var y = position.y;
-            var z = position.z;
-            
-            // Refer to CubeReference.png
-            var vertices = new List<Vector3>()
-            {
-                new Vector3(0, 0, 0),
-                new Vector3(1, 0, 0), // X
-                new Vector3(0, 1, 0), //   Y
-                new Vector3(1, 1, 0), // X Y
-                new Vector3(0, 0, 1), //     Z
-                new Vector3(1, 0, 1), // X   Z
-                new Vector3(0, 1, 1), //   Y Z
-                new Vector3(1, 1, 1), // X Y Z
-            };
-
-            // Triangles
-            var fullCube = new List<int>()
-            {
-                // -X face
-                4, 6, 0,
-                2, 0, 6,
-
-                // +X face
-                1, 3, 5,
-                7, 5, 3,
-                
-                // -Z face
-                0, 2, 1,
-                3, 1, 2,
-
-                // +Z face
-                5, 7, 4,
-                6, 4, 7,
-
-                // -Y face
-                0, 1, 4,
-                5, 4, 1,
-
-                // +Y face
-                3, 2, 7,
-                6, 7, 2,
-            };
-
-            var index = 0x0;
-            for (int i = 0; i < 6; i++)
-            {
-                if(VoxelData.UnpackData(neighbors[i].info).Item1 >= 8)
-                {
-                    index |= (byte)(1 << i);
-                }
-            }
-            
-            return new VoxelMeshRenderData(vertices, hiddenFaces[index]);
-                
-        }
-
-        private var hiddenFaces = new List<int>[]
+        private static List<int>[] HiddenFaces = new List<int>[]
         {
             new List<int>() {1, 3, 5, 7, 5, 3, 4, 6, 0, 2, 0, 6, 3, 2, 7, 6, 7, 2, 0, 1, 4, 5, 4, 1, 5, 7, 4, 6, 4, 7, 0, 2, 1, 3, 1, 2},
             new List<int>() {4, 6, 0, 2, 0, 6, 3, 2, 7, 6, 7, 2, 0, 1, 4, 5, 4, 1, 5, 7, 4, 6, 4, 7, 0, 2, 1, 3, 1, 2},
@@ -148,7 +78,78 @@ namespace VoxelEngine.Mesh
             new List<int>() {1, 3, 5, 7, 5, 3},
             new List<int>() { },
         };
+        
+        public override VoxelMeshRenderData GenerateVoxel(Vector3Int position, ref VoxelData voxel, VoxelData[] neighbors)
+        {
+            (byte, byte) data = VoxelData.UnpackData(voxel.info);
 
+            var intensity = data.Item1;
+            var materialIndex = data.Item2;
+            
+            if (intensity < 8)
+            {
+                return new VoxelMeshRenderData(new List<Vector3>(), new List<int>());
+            }
+
+            var x = position.x;
+            var y = position.y;
+            var z = position.z;
+            
+            // Refer to CubeReference.png
+            var vertices = new List<Vector3>()
+            {
+                new Vector3(0, 0, 0),
+                new Vector3(1, 0, 0), // X
+                new Vector3(0, 1, 0), //   Y
+                new Vector3(1, 1, 0), // X Y
+                new Vector3(0, 0, 1), //     Z
+                new Vector3(1, 0, 1), // X   Z
+                new Vector3(0, 1, 1), //   Y Z
+                new Vector3(1, 1, 1), // X Y Z
+            };
+
+            // Triangles
+            /*
+            var fullCube = new List<int>()
+            {
+                // -X face
+                4, 6, 0,
+                2, 0, 6,
+
+                // +X face
+                1, 3, 5,
+                7, 5, 3,
+                
+                // -Z face
+                0, 2, 1,
+                3, 1, 2,
+
+                // +Z face
+                5, 7, 4,
+                6, 4, 7,
+
+                // -Y face
+                0, 1, 4,
+                5, 4, 1,
+
+                // +Y face
+                3, 2, 7,
+                6, 7, 2,
+            };
+            */
+
+            var index = 0x0;
+            for (int i = 0; i < 6; i++)
+            {
+                if(VoxelData.UnpackData(neighbors[i].info).Item1 >= 8)
+                {
+                    index |= (byte)(0x1 << i);
+                }
+            }
+            
+            return new VoxelMeshRenderData(vertices, HiddenFaces[index]);
+                
+        }
     }
 
 }
