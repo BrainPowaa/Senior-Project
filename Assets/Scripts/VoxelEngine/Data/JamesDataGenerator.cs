5f727cdd4b7974053ab2b9086060008713a84356
inpurity190 <jsbny3@gmail.com>
2021-01-16 21:46:11 -0500

    Voxel Engine: Implemented FastNoise and improved terrain generation

---
+++ Assets/Scripts/VoxelEngine/Data/JamesDataGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VoxelEngine.Types;
+
+namespace VoxelEngine.Data
+{
+    [CreateAssetMenu(fileName = "JamesDataGenerator.asset", menuName = "VoxelEngine/Data Points/James Data Generator")]
+    public class JamesDataGenerator : VoxelDataGeneratorBase
+    {
+        public float offset = 1;
+        private FastNoise Noise = new FastNoise();
+        public override VoxelData CreateVoxelData(Vector3Int position)
+        {
+            var x = position.x * scale;
+            var y = position.y * scale;
+            var z = position.z * scale;
+
+            float intensity = Noise.GetValueFractal(x, y);
+            intensity += Noise.GetSimplex(y, z);
+            intensity += Noise.GetPerlin(x, z);
+            
+            intensity += Noise.GetValueFractal(y, x);
+            intensity += Noise.GetSimplex(z, y);
+            intensity += Noise.GetPerlin(z, x);
+            
+            intensity /= 6.0f;
+
+            intensity += offset;
+
+            return new VoxelData((byte)(intensity * 16), 0);
+        }
+    }
+}
