f821276cc2cb45fc50fed2a2dba34bef79eaff37
inpurity190 <jsbny3@gmail.com>
2021-01-16 23:51:17 -0500

    Voxel Engine: Using 3d Simplex Noise instead of 6x 2d noise functions

--- Assets/Scripts/VoxelEngine/Data/JamesDataGenerator.cs
+++ Assets/Scripts/VoxelEngine/Data/JamesDataGenerator.cs
@@ -7,29 +7,24 @@ using VoxelEngine.Types;
 namespace VoxelEngine.Data
 {
     [CreateAssetMenu(fileName = "JamesDataGenerator.asset", menuName = "VoxelEngine/Data Points/James Data Generator")]
+    
     public class JamesDataGenerator : VoxelDataGeneratorBase
     {
         public float offset = 1;
         private FastNoise Noise = new FastNoise();
         public override VoxelData CreateVoxelData(Vector3Int position)
         {
+            
+            
             var x = position.x * scale;
             var y = position.y * scale;
             var z = position.z * scale;
 
-            float intensity = Noise.GetValueFractal(x, y);
-            intensity += Noise.GetSimplex(y, z);
-            intensity += Noise.GetPerlin(x, z);
-            
-            intensity += Noise.GetValueFractal(y, x);
-            intensity += Noise.GetSimplex(z, y);
-            intensity += Noise.GetPerlin(z, x);
-            
-            intensity /= 6.0f;
+            float intensity = Noise.GetSimplexFractal(x, y, z);
 
             intensity += offset;
 
-            return new VoxelData((byte)(intensity * 16), 0);
+            return new VoxelData((byte)(intensity * 15), 0);
         }
     }
 }
