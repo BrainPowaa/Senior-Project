1f9fbec15a9984c6feca64e2cc34f476b23c21d1
inpurity190 <jsbny3@gmail.com>
2021-01-20 01:25:47 -0500

    Voxel Data Generator: Adding surface hills terrain and a height modifier, also added a second noise function to increase variation in hill patterns.

--- Assets/Scripts/VoxelEngine/Data/JamesDataGenerator.cs
+++ Assets/Scripts/VoxelEngine/Data/JamesDataGenerator.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using LibNoise;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using VoxelEngine.Types;
+using Math = System.Math;
 
 namespace VoxelEngine.Data
 {
@@ -18,14 +20,28 @@ namespace VoxelEngine.Data
         public float frequency = 2.3f;
         public float persistence = 1;
         public float lacunarity = 1;
-        private FastNoise noise = new FastNoise();
+        
+        public int surfaceBeginLevel = 64;
+        public float hillScale = 0.001f;
+        public float hillCutoffScale = 0.1f;
+        
+        public float rmfNoiseScale = 0.01f;
+        public float rmfHillScale = 2;
+        public float rmfDisplacement = 1;
+        public float rmfFrequency = 0.0001f;
+        public int rmfOctaveCount = 9;
+        
+        private Perlin noise = new Perlin();
+        private Perlin rmfNoise = new Perlin();
+
 
         public override float CreateVoxelData(Vector3Int position)
         {
             var x = position.x * scale;
             var y = position.y * scale;
             var z = position.z * scale;
-
+            
+            //Adjust settings of the noise function to simulate terrain in a more realistic manner
             noise.Frequency = frequency;
             noise.Lacunarity = lacunarity;
             noise.Persistence = persistence;
@@ -33,18 +49,22 @@ namespace VoxelEngine.Data
             noise.OctaveCount = octaveCount;
             noise.Seed = seed;
 
-            if (position.y > 128)
-            {
-                
-                float intensity = (float)noise.GetValue(x, y, z) * noiseScale;
+            rmfNoise.Seed = seed/2;
+            rmfNoise.Frequency = rmfFrequency;
+            rmfNoise.NoiseQuality = NoiseQuality.Low;
+            rmfNoise.OctaveCount = rmfOctaveCount;
+            
+            // Hills
+            var value = (((float) noise.GetValue(x, surfaceBeginLevel, z) * hillScale) -
+                         ((position.y - surfaceBeginLevel) * hillCutoffScale));
+            value += (((float) rmfNoise.GetValue(x, surfaceBeginLevel, z) * rmfHillScale) -
+                      ((position.y - surfaceBeginLevel) * rmfNoiseScale));
+            value = Math.Min(Math.Max(value, -1.0f), 1.0f);
+            
+            // Caves
+            value -= ((float)noise.GetValue(x, y, z) * noiseScale) + offset;
 
-                intensity += offset;
-
-                return intensity;
-                
-            }
-
-            return 0;
+            return value;
 
         }
     }
