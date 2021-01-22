696af4133c97a7f658fd6572e45327c436612c10
inpurity190 <jsbny3@gmail.com>
2021-01-22 01:55:23 -0500

    Voxel Data Generator: Vastly improved cave generation at a slight performance hit. Efficiency improvements coming soon.

--- Assets/Scripts/VoxelEngine/Data/JamesDataGenerator.cs
+++ Assets/Scripts/VoxelEngine/Data/JamesDataGenerator.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
 using LibNoise;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using VoxelEngine.Types;
 using Math = System.Math;
+using Perlin = LibNoise.Perlin;
 
 namespace VoxelEngine.Data
 {
@@ -14,57 +16,66 @@ namespace VoxelEngine.Data
     
     public class JamesDataGenerator : VoxelDataGeneratorBase
     {
-        public float offset = -0.55f;
-        public float noiseScale = 0.1f;
-        public int octaveCount = 1;
-        public float frequency = 2.3f;
-        public float persistence = 1;
-        public float lacunarity = 1;
         
-        public int surfaceBeginLevel = 64;
+        public int surfaceBeginLevel = 80;
         public float hillScale = 0.001f;
+        public float secondaryHillScale = 2.0f;
         public float hillCutoffScale = 0.1f;
+        public float secondaryHillNoiseScale = 0.01f;
+        public float caveNoiseScale = 1f;
+        public float caveFrequency = 1f;
+        public float caveCutoffLevel = 0.5f;
+        public float caveWarpScale = 1.0f;
         
-        public float rmfNoiseScale = 0.01f;
-        public float rmfHillScale = 2;
-        public float rmfDisplacement = 1;
-        public float rmfFrequency = 0.0001f;
-        public int rmfOctaveCount = 9;
+        private Perlin surfaceNoise1 = new Perlin();
+        private Perlin surfaceNoise2 = new Perlin();
+        private FastNoiseCave caveNoise1 = new FastNoiseCave();
+        private FastNoiseCave caveNoise2 = new FastNoiseCave();
         
-        private Perlin noise = new Perlin();
-        private Perlin rmfNoise = new Perlin();
-
-
         public override float CreateVoxelData(Vector3Int position)
         {
             var x = position.x * scale;
             var y = position.y * scale;
             var z = position.z * scale;
-            
-            //Adjust settings of the noise function to simulate terrain in a more realistic manner
-            noise.Frequency = frequency;
-            noise.Lacunarity = lacunarity;
-            noise.Persistence = persistence;
-            noise.NoiseQuality = NoiseQuality.Low;
-            noise.OctaveCount = octaveCount;
-            noise.Seed = seed;
 
-            rmfNoise.Seed = seed/2;
-            rmfNoise.Frequency = rmfFrequency;
-            rmfNoise.NoiseQuality = NoiseQuality.Low;
-            rmfNoise.OctaveCount = rmfOctaveCount;
-            
             // Hills
-            var value = (((float) noise.GetValue(x, surfaceBeginLevel, z) * hillScale) -
-                         ((position.y - surfaceBeginLevel) * hillCutoffScale));
-            value += (((float) rmfNoise.GetValue(x, surfaceBeginLevel, z) * rmfHillScale) -
-                      ((position.y - surfaceBeginLevel) * rmfNoiseScale));
-            value = Math.Min(Math.Max(value, -1.0f), 1.0f);
+            surfaceNoise1.Seed = seed;
+            surfaceNoise2.Seed = seed/2;
             
-            // Caves
-            value -= ((float)noise.GetValue(x, y, z) * noiseScale) + offset;
+            float hills = (((float)surfaceNoise1.GetValue(x, surfaceBeginLevel, z) * hillScale) -((position.y - surfaceBeginLevel) * hillCutoffScale));
+            hills += (((float) surfaceNoise2.GetValue(x, surfaceBeginLevel, z) * secondaryHillScale) -((position.y - surfaceBeginLevel) * secondaryHillNoiseScale));
+            hills = Math.Min(Math.Max(hills, -1.0f), 1.0f);
 
-            return value;
+            // Underground noise
+            //value -= ((float)noise.GetValue(x, y, z) * noiseScale) + offset;
+
+            // Caves
+            caveNoise1.SetSeed(seed);
+            caveNoise2.SetSeed(seed/2);
+            
+            caveNoise1.SetCellularJitter(0.3f);
+            caveNoise2.SetCellularJitter(0.3f);
+            
+            caveNoise1.SetFrequency(caveFrequency);
+            caveNoise2.SetFrequency(caveFrequency);
+            
+            caveNoise1.SetCellularReturnType(FastNoiseCave.CellularReturnType.Distance2Div);
+            caveNoise2.SetCellularReturnType(FastNoiseCave.CellularReturnType.Distance2Div);
+            
+            float caveNoiseValue1 = caveNoise1.GetCellular(x, y, z);
+            x += caveFrequency * 0.5f;
+            y += caveFrequency * 0.5f;
+            z += caveFrequency * 0.5f;
+            float caveNoiseValue2 = caveNoise2.GetCellular(x, y, z);
+
+            float caveNoiseValue = Math.Min(caveNoiseValue1, caveNoiseValue2);
+            //if (caveNoiseValue < caveCutoffLevel)
+                //return 0;
+
+            caveNoiseValue = Math.Min(Math.Max(caveNoiseValue, -1.0f), 1.0f);
+            float caves = (caveNoiseValue * caveNoiseScale);
+
+            return hills - caves;
 
         }
     }
