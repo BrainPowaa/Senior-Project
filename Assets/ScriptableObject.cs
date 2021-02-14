d1c32fd2743e8b11a92b190d6b34b66f667d6fa3
Parker Harris <parkharris@valdosta.edu>
2021-02-14 17:20:31 -0500

    Adding comments to the ScriptableOject Class

--- Assets/ScriptableObject.cs
+++ Assets/ScriptableObject.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
 public class SpawnManagerScriptableObject : ScriptableObject
 {
+    //This is the how we manage the ScriptableObjects that we create in unity. 
+
     public string prefabName;
 
     public int numberOfPrefabsToCreate;
