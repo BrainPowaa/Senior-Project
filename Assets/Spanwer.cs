707a03f626eeee3913350936cee5cba85b591e25
Parker Harris <parkharris@valdosta.edu>
2021-02-14 16:49:14 -0500

    Making a Script object to create items in the world.

---
+++ Assets/Spanwer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // The GameObject to instantiate.
+    public GameObject entityToSpawn;
+
+    // An instance of the ScriptableObject defined above.
+    public SpawnManagerScriptableObject spawnManagerValues;
+
+    // This will be appended to the name of the created entities and increment when each is created.
+    int instanceNumber = 1;
+
+    void Start()
+    {
+        SpawnEntities();
+    }
+
+    void SpawnEntities()
+    {
+        int currentSpawnPointIndex = 0;
+
+        for (int i = 0; i < spawnManagerValues.numberOfPrefabsToCreate; i++)
+        {
+            // Creates an instance of the prefab at the current spawn point.
+            GameObject currentEntity = Instantiate(entityToSpawn, spawnManagerValues.spawnPoints[currentSpawnPointIndex], Quaternion.identity);
+
+            // Sets the name of the instantiated entity to be the string defined in the ScriptableObject and then appends it with a unique number. 
+            currentEntity.name = spawnManagerValues.prefabName + instanceNumber;
+
+            // Moves to the next spawn point index. If it goes out of range, it wraps back to the start.
+            currentSpawnPointIndex = (currentSpawnPointIndex + 1) % spawnManagerValues.spawnPoints.Length;
+
+            instanceNumber++;
+        }
+    }
+}
