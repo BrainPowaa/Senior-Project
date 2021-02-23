127a5912ae435a9e366212c004f4ae053a20a949
brjBoN <brjbon@gmail.com>
2021-02-22 19:34:21 -0500

    Misc: Added improved third person camera

---
+++ Assets/Scenes/Braden's Closet/Scripts/testing 2/CameraOrbit.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{
+    public float lookSensitivity;
+    public float minXLook;
+    public float maxXLook;
+    public Transform camAnchor;
+
+    public bool invertXRotation;
+
+    private float curXRot;
+
+    private void LateUpdate()
+    {
+        float x = Input.GetAxis("Mouse X");
+        float y = Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles += Vector3.up * x * lookSensitivity;
+        if (invertXRotation)
+            curXRot += y * lookSensitivity;
+        else
+            curXRot -= y * lookSensitivity;
+
+        curXRot = Mathf.Clamp(curXRot, minXLook, maxXLook);
+
+        Vector3 clampedAngle = camAnchor.eulerAngles;
+        clampedAngle.x = curXRot;
+
+        camAnchor.eulerAngles = clampedAngle;
+    }
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
